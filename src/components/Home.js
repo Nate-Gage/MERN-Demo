80101c06a4c464f201bab7462a60690f08136da3
Nate <nate.d.gage@gmail.com>
2020-07-15 18:00:56 -0600

    Test file for Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Home() {
     return (
-        <div className="homePage">
+        <div className="homePage" data-test="homePage">
             <div className="container">
-                <h1 className="homeTitle">Welcome to your Wishlist!</h1>
+                <h1 className="homeTitle" data-test="header">Welcome to your Wishlist!</h1>
             </div>
         </div>
     )
 }
+
+Home.propTypes = {
+    header: PropTypes.string
+};
 
 export default Home;
---
+++ src/components/UnitTests/Home.test.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { shallow, EnzymeAdapter } from 'enzyme';
+import Home from '../Home';
+import { findByTestAttr, checkProps } from '../../../utils/index';
+import Adapter from 'enzyme-adapter-react-16';
+
+const setUp = (props={}) => {
+   const component = shallow(<Home {...props} />);
+   return component; 
+};
+
+describe('Home Component', () => {
+
+    describe('Check PropTypes', () => {
+
+        test('Should not throw warning', () => {
+            const expectedProps = {
+                header: 'Test header'
+            };
+
+            const propsErr = checkProps(Home, expectedProps);
+            expect(propsErr).toBe(undefined);
+        });
+    });
+
+    describe('Have props', () => {
+
+        let wrapper;
+        beforeEach(() => {
+            const testProps = {
+                header: 'test header'
+            };
+
+            wrapper = setUp(testProps);
+        })
+
+        test('Should render <h1> without errors', () => {
+            const h1 = findByTestAttr(wrapper, 'header');
+            expect(h1.length).toBe(1);
+        })
+    });
+
+    describe('Have no props', () => {
+
+        let wrapper;
+        beforeEach(() => {
+            wrapper = setUp();
+        })
+
+        test('Should not render', () => {
+            const component = findByTestAttr(wrapper, 'header');
+            expect(component.length).toBe(0);
+        })
+    });
+
+});
